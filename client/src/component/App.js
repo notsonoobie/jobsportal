421ebabd8e7e027a6a59b597a2e35fb07405dc5d
Rahul Gupta <swastikmedical74@gmail.com>
2020-09-20 04:38:10 +0530

    Implemented React-Router Functionality and build basic skeleton of pages

--- client/src/component/App.js
+++ client/src/component/App.js
@@ -1,10 +1,22 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import Home from './Pages/Home'
+import Register from './Pages/Register'
+import About from './Pages/About'
+import Login from './Pages/Login'
+import ErrorPage from './Pages/ErrorPage'
 import '../css/App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>hello</h1>
+      <Switch>
+        <Route exact path="/about" render={()=> <About />} />
+        <Route exact path="/register" render={()=> <Register />} />
+        <Route exact path="/login" render={()=> <Login />} />
+        <Route exact path="/" render={()=> <Home />} />
+        <Route render={()=> <ErrorPage />} />
+      </Switch>
     </div>
   );
 }
